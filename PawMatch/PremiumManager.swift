2921789e0609327af0750b836d35e9e6b132f1cd
pawmatchadmin <pmgvpqbnk5@privaterelay.appleid.com>
2025-12-16 16:02:18 -0500

    Update PremiumManager.swift
    Persist premium state using UserDefaults

--- PawMatch/PremiumManager.swift
+++ PawMatch/PremiumManager.swift
@@ -3,20 +3,28 @@ import Foundation
 final class PremiumManager {
     static let shared = PremiumManager()
 
-    private init() {}
+    private let premiumKey = "isPremiumUser"
+
+    private init() {
+        // Load saved premium state on launch
+        isPremium = UserDefaults.standard.bool(forKey: premiumKey)
+    }
 
     private(set) var isPremium: Bool = false
 
     func enablePremium() {
         isPremium = true
+        UserDefaults.standard.set(true, forKey: premiumKey)
     }
 
     func disablePremium() {
         isPremium = false
+        UserDefaults.standard.set(false, forKey: premiumKey)
     }
 
     func restorePremium() {
-        // to be implemented with StoreKit restore
-        isPremium = false
+        // This will later be replaced by StoreKit restore logic
+        let savedValue = UserDefaults.standard.bool(forKey: premiumKey)
+        isPremium = savedValue
     }
 }
